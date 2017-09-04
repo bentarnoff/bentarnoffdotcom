2967fed946b10979131358f69f665b143d570f22
Ben Tarnoff <ben@bentarnoff.com>
2017-09-04 11:10:52 -0400

    use github for assets

--- bentarnoff/settings.py
+++ bentarnoff/settings.py
@@ -122,7 +122,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'bentarnoff'
 
-STATIC_URL = 'http://bentarnoff.s3.amazonaws.com/static/'
+STATIC_URL = 'https://bentarnoff.github.io/tarnoff-static/bentarnoff/static/'
 
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
